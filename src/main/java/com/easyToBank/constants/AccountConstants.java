8dcf7346f4d8a8f3504a037094bd9d38d5ac4ae0
satishmarutham <satishlakshmiudemy@gmail.com>
2024-01-28 13:04:32 +0530

    Space

--- src/main/java/com/easyToBank/constants/AccountConstants.java
+++ src/main/java/com/easyToBank/constants/AccountConstants.java
@@ -4,7 +4,7 @@ public class AccountConstants {
     public static final String SAVINGS = "Savings";
     public static final String ADDRESS = "Bangalore ElectronCity Phase-2";
     public static final String STATUS_201 = "201";
-    public static final String MESSAGE_201 = "Account Created Successfully";
+    public static final String MESSAGE_201 = "Account Created Successfully ";
     public static final String STATUS_200 = "200";
     public static final String MESSAGE_200 = "Request Processed Successfully";
     public static final String STATUS_417 = "417";
