faeaa8ae6af293345ad6f5a34e9c9c2b4397e8de
satishmarutham <satishlakshmiudemy@gmail.com>
2024-02-07 20:43:22 +0530

    Space

--- src/main/java/com/easyToBank/constants/AccountConstants.java
+++ src/main/java/com/easyToBank/constants/AccountConstants.java
@@ -5,11 +5,6 @@ public class AccountConstants {
     public static final String ADDRESS = "Bangalore ElectronCity Phase-2";
     public static final String STATUS_201 = "201";
     public static final String MESSAGE_201 = "Account Created Successfully ";
-    public static final String STATUS_200 = "200";
-    public static final String MESSAGE_200 = "Request Processed Successfully";
-    public static final String STATUS_417 = "417";
-    public static final String MESSAGE_417_UPDATE = "Update operation failed. Please try again or contact Dev team";
-    public static final String MESSAGE_417_DELETE = "Delete operation failed. Please try again or contact Dev team";
     private AccountConstants() {
 
     }
